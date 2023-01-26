107bb647289ae1c02e5296114201589a9a1e71d3
Suar <fn7sephiroth@gmail.com>
2023-01-26 17:40:23 +0100

    Más ViewBinding, login provisional, inicio home, custom back key en login, eliminando toolbar

--- app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/view/LoginFragment.kt
+++ app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/view/LoginFragment.kt
@@ -1,13 +1,14 @@
 package net.iessochoa.suarpl.suarquizcodeapp.view
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import net.iessochoa.suarpl.suarquizcodeapp.R
 import net.iessochoa.suarpl.suarquizcodeapp.databinding.FragmentLoginBinding
@@ -15,16 +16,25 @@ import net.iessochoa.suarpl.suarquizcodeapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    //Implementando viewBinding
+    //Variables ViewBinding
     private var _binding:FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //ToDo IMPLEMENTAR CONFIRMACIÓN DE SALIDA
+        /*Asignando al botón back una acción personalizada
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Toast.makeText(activity, "Test back", Toast.LENGTH_LONG).show()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+        */
 
     }
-
+    //Metemos ViewBinding al fragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,9 +46,25 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //Probando binding
-        binding.buttonLogin.setOnClickListener{
+
+
+        //Probando ViewBinding
+
+        //TODO CAMBIAR ESTE LOGIN PROVISIONAL EN FIREBASE E INTEGRAR EN MVVM
+        binding.tvRegister.setOnClickListener{
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
             }
+        binding.buttonLogin.setOnClickListener{
+            var password = binding.editTextTextPassword.text.toString()
+            var mail = binding.editTextTextEmailAddress.text.toString()
+
+            if (mail == "suar@prueba.com" && password == "1234"){
+                findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            }else{
+                Toast.makeText(activity, "Usuario no registrado", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 }
