ee64e910efb3877709409397da87e7ef71429c4b
Suar <fn7sephiroth@gmail.com>
2023-01-27 02:37:01 +0100

    Botones de Home, salir

--- app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/view/HomeFragment.kt
+++ app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/view/HomeFragment.kt
@@ -1,13 +1,14 @@
 package net.iessochoa.suarpl.suarquizcodeapp.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import net.iessochoa.suarpl.suarquizcodeapp.R
+import android.widget.Toast
 import net.iessochoa.suarpl.suarquizcodeapp.databinding.FragmentHomeBinding
-import net.iessochoa.suarpl.suarquizcodeapp.databinding.FragmentLoginBinding
+import kotlin.system.exitProcess
 
 class HomeFragment : Fragment() {
     //Variables ViewBinding
@@ -29,5 +30,27 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btLogout.setOnClickListener{
+            /*Construyendo AlertDialog para salir de la aplicación
+            al pulsar en botón salir*/
+            //TODO IMPLEMENTAR CIERRE DE CONEXIÓN EN UN FUTURO
+            val salirDialog = AlertDialog.Builder(activity)
+            salirDialog.setMessage("¿Quieres salir de la aplicación ???")
+                .setCancelable(false)
+                .setPositiveButton("Sí") { _, _ -> exitProcess(0) }
+                .setNegativeButton("No") { dialog, _ -> dialog.cancel() }
+            val alert = salirDialog.create()
+            alert.setTitle("Dialog Header")
+            alert.show()
+        }
+
+        binding.btRanking.setOnClickListener {
+            //Mostrar el ranking
+            //TODO FEATURE OPCIONAL, IMPLEMENTAR SI DA TIEMPO
+            Toast.makeText(activity, "Feature opcional, aún no implementada", Toast.LENGTH_LONG).show()
+
+        }
+
     }
 }
