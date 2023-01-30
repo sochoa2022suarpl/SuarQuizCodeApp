8354360b36d8df14d306be025d2582f0079f92b2
Suar <fn7sephiroth@gmail.com>
2023-01-30 18:35:40 +0100

    Integrado sistema de dificultad con safeargs, pasando el tiempo restante entre fragmentos

--- app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/view/HomeFragment.kt
+++ app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/view/HomeFragment.kt
@@ -1,14 +1,19 @@
 package net.iessochoa.suarpl.suarquizcodeapp.view
 
 import android.app.AlertDialog
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import net.iessochoa.suarpl.suarquizcodeapp.R
 import net.iessochoa.suarpl.suarquizcodeapp.adapter.CategoryAdapter
 import net.iessochoa.suarpl.suarquizcodeapp.databinding.FragmentHomeBinding
 import net.iessochoa.suarpl.suarquizcodeapp.model.QzCategory
@@ -25,6 +30,9 @@ class HomeFragment : Fragment() {
     private var qzCategoryMutableList: MutableList<QzCategory> = QzCategoryProvider.qzCategoryList.toMutableList()
     private val llmanager = LinearLayoutManager(context)
     private lateinit var adapter: CategoryAdapter
+
+    //Segundos restantes que luego pasaremos al fragmento del cuestionario
+    private var secondsLeft:Int = 100;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,13 +65,45 @@ class HomeFragment : Fragment() {
             exitApp()
 
         }
-
+        /*
         binding.btRanking.setOnClickListener {
             //Mostrar el ranking
             //TODO FEATURE OPCIONAL, IMPLEMENTAR SI DA TIEMPO
             Toast.makeText(activity, "Feature opcional, aún no implementada", Toast.LENGTH_LONG).show()
+        }
+         */
 
 
+        //ToDo AÑADIR ONCLICKLISTENER EN EL ADAPTADOR DEL RECYCLERVIEW
+        /*Navegación provisional en este botón hasta el fragmento cuestionario hasta integrar
+        onclicklistener en las diferentes opciones de la MutableList y la BD. Mediante safeargs
+        pasamos el valor de los segundos restantes al siguiente fragmento
+        */
+        binding.apply {
+            binding.btRanking.setOnClickListener{
+                val next = HomeFragmentDirections.actionHomeFragmentToQuizFragment(secondsLeft)
+                findNavController().navigate(next)
+            }
+        }
+        /*
+        Al cambiar la dificultad con los radiobuttons, cambia el color
+        del radiobutton y cambia la variable que contiene los segundos restantes
+        que pasaremos al fragmento del cuestionario
+         */
+        binding.rgDifficulty.setOnCheckedChangeListener { group, i ->
+            when(i){
+                binding.rbNormal.id -> {
+                    binding.rgDifficulty.setBackgroundResource(R.drawable.radiusgreen)
+                    secondsLeft = 100;
+                    Snackbar.make(binding.homeFragment, "Tiempo total fijado a 100 segundos", Snackbar.LENGTH_LONG).show()
+                }
+                binding.rbDificil.id -> {
+                    binding.rgDifficulty.setBackgroundResource(R.drawable.radiusred)
+                    secondsLeft=60;
+                    Snackbar.make(binding.homeFragment, "Tiempo total fijado a 60 segundos", Snackbar.LENGTH_LONG).show()
+
+                }
+            }
         }
 
     }
