91b4765b7bdb84f4aa863bf69ef04f5b16f29920
Suar <fn7sephiroth@gmail.com>
2023-01-31 02:09:44 +0100

    Inicio entrega2

---
+++ app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/model/QuestionModel.kt
@@ -0,0 +1,11 @@
+package net.iessochoa.suarpl.suarquizcodeapp.model
+
+//Data class de pregunta
+data class QuestionModel(
+    val question:String,
+    val option1:String,
+    val option2:String,
+    val option3:String,
+    val option4:String,
+    val answer:String
+)
