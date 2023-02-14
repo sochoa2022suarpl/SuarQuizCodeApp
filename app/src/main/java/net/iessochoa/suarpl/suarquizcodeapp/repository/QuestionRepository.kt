639ba77ebc5c534b8a627646e9af620dbc968c3c
Suar <fn7sephiroth@gmail.com>
2023-02-14 01:13:02 +0100

    Implementado fetch de preguntas desde Firebase

--- app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/repository/QuestionRepository.kt
+++ app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/repository/QuestionRepository.kt
@@ -1,4 +1,43 @@
 package net.iessochoa.suarpl.suarquizcodeapp.repository
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import net.iessochoa.suarpl.suarquizcodeapp.model.QuestionModel
+
 class QuestionRepository {
+    private var questionModelRepositoryListKotlin = ArrayList<QuestionModel>()
+
+
+    fun getFirebaseQuestions(category: String): LiveData<MutableList<QuestionModel>> {
+        val mutableData = MutableLiveData<MutableList<QuestionModel>>()
+        FirebaseFirestore.getInstance().collection(category)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    val listData = mutableListOf<QuestionModel>()
+                    for (document in querySnapshot) {
+                        val pregunta = document.get("question").toString()
+                        val opcion1 = document.get("option1").toString()
+                        val opcion2 = document.get("option2").toString()
+                        val opcion3 = document.get("option3").toString()
+                        val opcion4 = document.get("option4").toString()
+                        val respuesta = document.get("answer").toString()
+
+                        val question =
+                            QuestionModel(pregunta, opcion1, opcion2, opcion3, opcion4, respuesta)
+                        listData.add(question)
+
+                    }
+                    mutableData.value = listData
+                }
+            }
+        return mutableData
+
+
+    }
 }
--- app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/viewmodel/QuestionViewModel.kt
+++ app/src/main/java/net/iessochoa/suarpl/suarquizcodeapp/viewmodel/QuestionViewModel.kt
@@ -1,9 +1,11 @@
 package net.iessochoa.suarpl.suarquizcodeapp.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import net.iessochoa.suarpl.suarquizcodeapp.model.QuestionModel
 import net.iessochoa.suarpl.suarquizcodeapp.model.QuestionModelProvider
+import net.iessochoa.suarpl.suarquizcodeapp.repository.QuestionRepository
 
 
 class QuestionViewModel: ViewModel() {
@@ -11,14 +13,24 @@ class QuestionViewModel: ViewModel() {
     private var questionModelListData = MutableLiveData<List<QuestionModel>>()
 
     //Obtiene una lista de objetos dependiendo de la categoría seleccionada, provisional hasta integrar Repositorio
-    fun getLiveDataFromCategory(selectedCategory: String): MutableLiveData<List<QuestionModel>>{
-        when(selectedCategory){
-            "KOTLIN" -> questionModelListData = MutableLiveData<List<QuestionModel>>(QuestionModelProvider.questionModelProviderListKotlin)
-            "JAVA" -> questionModelListData = MutableLiveData<List<QuestionModel>>(QuestionModelProvider.questionModelProviderListJava)
-            "REACT" -> questionModelListData = MutableLiveData<List<QuestionModel>>(QuestionModelProvider.questionModelProviderListReact)
+    fun getLiveDataFromCategory(selectedCategory: String): LiveData<MutableList<QuestionModel>> {
+        var mutableData = MutableLiveData<MutableList<QuestionModel>>()
+        val repo = QuestionRepository()
 
-        }
-        return questionModelListData
+        when(selectedCategory){
+    "KOTLIN" ->         repo.getFirebaseQuestions("KOTLINQUESTIONS").observeForever {
+        mutableData.value = it
+    }
+    "JAVA" ->         repo.getFirebaseQuestions("JAVAQUESTIONS").observeForever {
+        mutableData.value = it
+    }
+    "REACT" ->         repo.getFirebaseQuestions("REACTQUESTIONS").observeForever {
+        mutableData.value = it
+    }
+
+}
+
+        return mutableData
     }
 
 
